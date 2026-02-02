1135b2bcce714fb4334e91e3962574be9c78949c
naruapon <naruapon@gmail.com>
2026-02-02 10:14:27 +0700

    up

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
+//up
 export default defineConfig({
   base: '/react-app/',
   plugins: [react()],
